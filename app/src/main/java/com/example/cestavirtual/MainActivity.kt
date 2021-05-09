f5c8128a07b48d405a044741d97683e79936ab8f
jefe <jefe@gmail.com>
2021-05-09 15:38:21 +0200

    Primer commit

---
+++ app/src/main/java/com/example/cestavirtual/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.cestavirtual
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController:NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        actionBar?.title="Cesta Virtual"
+
+    }
+
+}
